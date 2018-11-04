7bfb935d8dd26926302311d0017827ec864fd05e
DavidGoedicke <2676840+DavidGoedicke@users.noreply.github.com>
2018-11-04 16:03:16 -0500

    added basic autonomation

---
+++ Assets/overwritesteering.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overwritesteering : MonoBehaviour {
+    public waypoint startWaypoint;
+	// Use this for initialization
+	void Start () {
+        VehicleController vc = GameObject.FindObjectOfType<VehicleController>();
+        vc.transform.GetComponent<VehicleInputController>().enabled = false;
+        vc.transform.GetComponent<VehiclePathController>().enabled = false;
+        waypointMovementManagerV2 wmm = vc.transform.gameObject.AddComponent<waypointMovementManagerV2>();
+        wmm.startWaypoint=startWaypoint;
+        vc.transform.gameObject.AddComponent<AIInput>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
